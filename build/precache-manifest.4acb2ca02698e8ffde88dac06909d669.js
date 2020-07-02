a459337b14461c1fff05728585b0a37a4419c7c1
mhuggi <emil.e.salmi@gmail.com>
2020-07-02 14:57:15 +0300

    added build

---
+++ build/precache-manifest.4acb2ca02698e8ffde88dac06909d669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304c832fae5f9cfeab5d8078b8769ec1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f765f0cb8efb7c58ac69",
+    "url": "/static/css/main.61354910.chunk.css"
+  },
+  {
+    "revision": "3cecff66644f73fc80c9",
+    "url": "/static/js/2.357f7ab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.357f7ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f765f0cb8efb7c58ac69",
+    "url": "/static/js/main.a37af028.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 
-app.use(express.static('build'))
 app.use(cors())
 app.use(express.json()) 
+app.use(express.static('build'))
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 
